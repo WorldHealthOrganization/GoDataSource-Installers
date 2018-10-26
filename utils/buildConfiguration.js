182e56ce5a9433cc6cd5ff5f9913350fe622409a
Andrei Bouariu <andrei.bouariu@clarisoft.com>
2018-10-26 11:17:17 +0300

    Build configuration for production s11

--- utils/buildConfiguration.js
+++ utils/buildConfiguration.js
@@ -5,6 +5,9 @@
  * whose values can be used by the Go.Data low level API to set the build type and platform
  * @param type - String: 'hub' or 'consolidation'
  */
+
+const { ARCH, MONGO_PLATFORM } = require('./../package')
+
 const mapTypeToBuildConfiguration = (type) => {
     let result = {}
 
@@ -16,25 +19,27 @@ const mapTypeToBuildConfiguration = (type) => {
             result.type = 'hub'
             break
     }
-    switch (process.env.MONGO_PLATFORM) {
+    let dependency = process.env.NODE_ENV === 'development' ? process.env.MONGO_PLATFORM : MONGO_PLATFORM
+    switch (dependency) {
         case 'darwin':
-            result.platform = `mac_osx-${process.env.ARCH}`
+            result.platform = `mac_osx-${process.env.NODE_ENV === 'development' ? process.env.ARCH : ARCH}`
             break
         case 'win':
-            result.platform = `windows-${process.env.ARCH}`
+            result.platform = `windows-${process.env.NODE_ENV === 'development' ? process.env.ARCH : ARCH}`
             break
         case 'deb':
-            result.platform = `linux_debian-${process.env.ARCH}`
+            result.platform = `linux_debian-${process.env.NODE_ENV === 'development' ? process.env.ARCH : ARCH}`
             break
         case 'rhel':
-            result.platform = `linux_rhel-${process.env.ARCH}`
+            result.platform = `linux_rhel-${process.env.NODE_ENV === 'development' ? process.env.ARCH : ARCH}`
             break
         default:
-            result.platform = `windows-${process.env.ARCH}`
+            result.platform = `windows-${process.env.NODE_ENV === 'development' ? process.env.ARCH : ARCH}`
             break
     }
     return result
 }
+
 
 module.exports = {
     mapTypeToBuildConfiguration
