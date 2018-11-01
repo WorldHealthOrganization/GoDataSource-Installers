98c9985ef38e2b36e01554e72e2a4ab379da849e
Andrei Bouariu <andrei.bouariu@clarisoft.com>
2018-11-01 08:54:30 -0400

    Completed linux cli with db migration

--- go-data-x64.sh
+++ go-data-x64.sh
@@ -36,22 +36,43 @@ platforms/linux/${ARCH}/default/node/bin/node go-data/build/installer/common/con
 platforms/linux/${ARCH}/default/node/bin/node go-data/build/installer/common/config set buildType ${APP_TYPE}
 platforms/linux/${ARCH}/default/node/bin/node go-data/build/installer/common/config set buildPlatform ${ARCH}
 
+#perform cleanup
+echo "Stopping PM2 process..."
+app-management/bin/pm2 stop server
+
+echo "Stopping process on port ${MONGO_PORT}..."
+kill -9 $(lsof -t -i:${MONGO_PORT})
+
+echo "Stopping process on port ${GODATA_PORT}..."
+kill -9 $(lsof -t -i:${GODATA_PORT})
+
 #create path for Mongo
 mkdir -p ${DBPATH}/data
 mkdir -p ${DBPATH}/logs
 
 #start Mongo
+echo "Starting Mongo process on port ${MONGO_PORT}..."
 platforms/linux/${ARCH}/default/mongodb/bin/mongod --dbpath ${DBPATH}/data --port=${MONGO_PORT} --fork --logpath=${DBPATH}/logs/db.log
 
 #check file with app version
 VERSION_PATH=${DBPATH}/.appVersion
 if [ ! -f ${VERSION_PATH} ]; then
     #perform database population
+    echo "Populating database..."
+    platforms/linux/${ARCH}/default/node/bin/node go-data/build/installer/common/config get version > ${DBPATH}/.appVersion
     platforms/linux/${ARCH}/default/node/bin/node go-data/build/server/install/install.js init-database
 else
     #perform database migration if version is different
     settings_version=$(cat "$VERSION_PATH")
-    echo "Database migration is not handled."
+    echo "SETTINGS VERSION ${settings_version}"
+    app_version=$(platforms/linux/${ARCH}/default/node/bin/node go-data/build/installer/common/config get version)
+    echo "APP VERSION ${app_version}"
+    if [ "$settings_version" = "$app_version" ]; then
+        echo "Database migration not needed."
+    else
+        echo "Migrating database from version ${settings_version} to ${app_version}..."
+        platforms/linux/${ARCH}/default/node/bin/node go-data/build/server/install/install.js migrate-database from ${settings_version} to ${app_version}
+    fi
 fi
 
 #start Go.Data
