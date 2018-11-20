5a2bf538c211f94723c23989d07e5db6e0eaceb9
Andrei Bouariu <andrei.bouariu@clarisoft.com>
2018-11-20 17:24:35 +0200

    Replaced ncp with fs-extra due to ncp callback called multiple times

--- build-scripts/move-build.js
+++ build-scripts/move-build.js
@@ -1,20 +1,32 @@
-const ncp = require('ncp').ncp
+const fs = require('fs-extra')
+const rimraf = require('rimraf')
 
 /**
+ * Remove web app production directory ("go-data/build")
  * Copy frontend production build to API build client directory ("go-data/frontend/dist" to "go-data/api/build/client")
  * Copy API build directory to go-data submodules directory ("go-data/api/build/client" to "go-data"
  */
-ncp('./../go-data/frontend/dist', './../go-data/api/build/client/dist', (err) => {
-    if (err) {
-        output(JSON.stringify(err), true)
-    }
-    ncp('./../go-data/api/build', './../go-data/build', (err) => {
+function build() {
+    console.log('Removing build folder...')
+    rimraf('./../go-data/build', (err) => {
         if (err) {
-            output(JSON.stringify(err), true)
+            return output(JSON.stringify(err), true)
         }
-        output('Successfully copied build folder', false)
+        console.log('Copying frontend build...')
+        fs.copy('./../go-data/frontend/dist', './../go-data/api/build/client/dist', { overwrite: true }, (err) => {
+            if (err) {
+                return output(JSON.stringify(err), true)
+            }
+            console.log('Copying API build...')
+            fs.copy('./../go-data/api/build', './../go-data/build', { overwrite: true}, (err) => {
+                if (err) {
+                    return output(JSON.stringify(err), true)
+                }
+                output('Successfully copied build folder.', false)
+            })
+        })
     })
-})
+}
 
 /**
  * Output a message to stdout/stderr and end process
@@ -26,3 +38,6 @@ function output(message, isError) {
     process[isError ? 'stderr' : 'stdout'].write(`${message}\n`);
     process.exit(isError ? 1 : 0);
 }
+
+// Start build
+build()
