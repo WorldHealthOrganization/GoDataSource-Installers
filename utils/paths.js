4ff614bb5ea7f42b0c77005cc4f1856040a32ddf
Adrian Morariu <adrian.morariu@clarisoft.com>
2019-11-01 11:17:01 +0200

    WGD-2462 After implementation of services there are problems installing and starting Go.Data

--- utils/paths.js
+++ utils/paths.js
@@ -1,62 +1,62 @@
-const {app} = require('electron')
-const path = require('path')
+const {app} = require('electron');
+const path = require('path');
 
-const desktopAppPackage = require('./../package')
-const {NODE_PLATFORM, MONGO_PLATFORM, ARCH, OSVERSION} = require('./../package')
+const desktopAppPackage = require('./../package');
+const {NODE_PLATFORM, MONGO_PLATFORM, ARCH, OSVERSION} = require('./../package');
 
-const appDirectory = app.getPath('userData')
-const dbDirectory = path.join(app.getPath('userData'), 'db')
-const dbLogDirectory = path.join(app.getPath('userData'), 'logs/db')
-const dbLogPath = path.join(dbLogDirectory, 'db.log')
+const appDirectory = app.getPath('userData');
+const dbDirectory = path.join(app.getPath('userData'), 'db');
+const dbLogDirectory = path.join(app.getPath('userData'), 'logs/db');
+const dbLogPath = path.join(dbLogDirectory, 'db.log');
 
-const appLogDirectory = path.join(app.getPath('userData'), 'logs/app')
-const appLogPath = path.join(appLogDirectory, 'web-app.log')
+const appLogDirectory = path.join(app.getPath('userData'), 'logs/app');
+const appLogPath = path.join(appLogDirectory, 'web-app.log');
 
-const testEnctryptionDirectory = path.join(app.getPath('userData'), 'logs/TEST')
+const testEnctryptionDirectory = path.join(app.getPath('userData'), 'logs/TEST');
 
-const resourceDirectory = path.join(__dirname, './../resources')
-const windowsDirectory = path.join(__dirname, './../windows')
+const resourceDirectory = path.join(__dirname, './../resources');
+const windowsDirectory = path.join(__dirname, './../windows');
 
-let webAppDirectory = undefined
-let mongodPath = undefined
-let mongoPath = undefined
-let mongoDirectory = undefined
-let nodePath = undefined
-let nssmPath = undefined
-let pm2Module = undefined
-let dbScript = undefined
-let backupScript = undefined
-let configScript = undefined
+let webAppDirectory = undefined;
+let mongodPath = undefined;
+let mongoPath = undefined;
+let mongoDirectory = undefined;
+let nodePath = undefined;
+let nssmPath = undefined;
+let pm2Module = undefined;
+let dbScript = undefined;
+let backupScript = undefined;
+let configScript = undefined;
 if (process.env.NODE_ENV === 'development') {
-    webAppDirectory = path.join(__dirname, './../go-data/build')
-    mongodPath = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin/mongod${ process.env.MONGO_PLATFORM === 'win' ? '.exe' : '' }`)
-    mongoPath = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin/mongo${ process.env.MONGO_PLATFORM === 'win' ? '.exe' : '' }`)
-    mongoDirectory = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin`)
-    nodePath = path.join(__dirname, `./../platforms/${process.env.NODE_PLATFORM}/${process.env.ARCH}/default/node${process.env.NODE_PLATFORM !== 'win' ? '/bin' : ''}/node`)
-    nssmPath = path.join(__dirname, `./../platforms/${process.env.NODE_PLATFORM}/${process.env.ARCH}/default/nssm/nssm.exe`)
-    pm2Module = process.platform === 'win32' ? path.join(__dirname, './../app-management/node_modules/pm2') : path.join(__dirname, './../app-management/lib/node_modules/pm2')
-    dbScript = path.join(__dirname, './../go-data/build/server/install/install.js')
-    backupScript = path.join(__dirname, './../go-data/build/server/scripts/restoreBackup.js')
-    configScript = path.join(__dirname, './../go-data/build/installer/common/config.js')
+    webAppDirectory = path.join(__dirname, './../go-data/build');
+    mongodPath = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin/mongod${ process.env.MONGO_PLATFORM === 'win' ? '.exe' : '' }`);
+    mongoPath = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin/mongo${ process.env.MONGO_PLATFORM === 'win' ? '.exe' : '' }`);
+    mongoDirectory = path.join(__dirname, `./../platforms/${process.env.MONGO_PLATFORM}/${process.env.ARCH}/${process.env.OSVERSION}/mongodb/bin`);
+    nodePath = path.join(__dirname, `./../platforms/${process.env.NODE_PLATFORM}/${process.env.ARCH}/default/node${process.env.NODE_PLATFORM !== 'win' ? '/bin' : ''}/node`);
+    nssmPath = path.join(__dirname, `./../platforms/${process.env.NODE_PLATFORM}/${process.env.ARCH}/default/nssm/nssm.exe`);
+    pm2Module = process.platform === 'win32' ? path.join(__dirname, './../app-management/node_modules/pm2') : path.join(__dirname, './../app-management/lib/node_modules/pm2');
+    dbScript = path.join(__dirname, './../go-data/build/server/install/install.js');
+    backupScript = path.join(__dirname, './../go-data/build/server/scripts/restoreBackup.js');
+    configScript = path.join(__dirname, './../go-data/build/installer/common/config.js');
 } else {
-    webAppDirectory = path.join(process.resourcesPath, 'go-data/build')
-    mongodPath = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin/mongod${ MONGO_PLATFORM === 'win' ? '.exe' : '' }`)
-    mongoPath = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin/mongo${ MONGO_PLATFORM === 'win' ? '.exe' : '' }`)
-    mongoDirectory = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin`)
-    nodePath = path.join(process.resourcesPath, `./platforms/${NODE_PLATFORM}/${ARCH}/default/node${NODE_PLATFORM !== 'win' ? '/bin' : ''}/node`)
-    nssmPath = path.join(process.resourcesPath, `./platforms/${NODE_PLATFORM}/${ARCH}/default/nssm/nssm.exe`)
-    pm2Module = process.platform === 'win32' ?  path.join(process.resourcesPath, './app-management/node_modules/pm2') : path.join(process.resourcesPath, './app-management/lib/node_modules/pm2')
-    dbScript = path.join(process.resourcesPath, './go-data/build/server/install/install.js')
-    backupScript = path.join(process.resourcesPath, './go-data/build/server/scripts/restoreBackup.js')
-    configScript = path.join(process.resourcesPath, './go-data/build/installer/common/config.js')
+    webAppDirectory = path.join(process.resourcesPath, 'go-data/build');
+    mongodPath = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin/mongod${ MONGO_PLATFORM === 'win' ? '.exe' : '' }`);
+    mongoPath = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin/mongo${ MONGO_PLATFORM === 'win' ? '.exe' : '' }`);
+    mongoDirectory = path.join(process.resourcesPath, `./platforms/${MONGO_PLATFORM}/${ARCH}/${OSVERSION}/mongodb/bin`);
+    nodePath = path.join(process.resourcesPath, `./platforms/${NODE_PLATFORM}/${ARCH}/default/node${NODE_PLATFORM !== 'win' ? '/bin' : ''}/node`);
+    nssmPath = path.join(process.resourcesPath, `./platforms/${NODE_PLATFORM}/${ARCH}/default/nssm/nssm.exe`);
+    pm2Module = process.platform === 'win32' ?  path.join(process.resourcesPath, './app-management/node_modules/pm2') : path.join(process.resourcesPath, './app-management/lib/node_modules/pm2');
+    dbScript = path.join(process.resourcesPath, './go-data/build/server/install/install.js');
+    backupScript = path.join(process.resourcesPath, './go-data/build/server/scripts/restoreBackup.js');
+    configScript = path.join(process.resourcesPath, './go-data/build/installer/common/config.js');
 }
 
-const pm2File = path.join(pm2Module, 'bin/pm2')
-const webAppLaunchScript = path.join(webAppDirectory, 'server/server.js')
-const webAppPackage = path.join(webAppDirectory, 'package')
-const webAppVersion = require(webAppPackage).version
-const webAppInstalledVersion = path.join(app.getPath('userData'), '.appVersion')
-const settingsFile = path.join(app.getPath('userData'), '.settings')
+const pm2File = path.join(pm2Module, 'bin/pm2');
+const webAppLaunchScript = path.join(webAppDirectory, 'server/server.js');
+const webAppPackage = path.join(webAppDirectory, 'package');
+const webAppVersion = require(webAppPackage).version;
+const webAppInstalledVersion = path.join(app.getPath('userData'), '.appVersion');
+const settingsFile = path.join(app.getPath('userData'), '.settings');
 
 module.exports = {
     appDirectory: appDirectory,
@@ -89,4 +89,4 @@ module.exports = {
     resourcesDirectory: resourceDirectory,
     windowsDirectory: windowsDirectory,
     testEncryptionDirectory: testEnctryptionDirectory
-}
+};
